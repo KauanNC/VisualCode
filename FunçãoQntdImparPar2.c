814bdc72975f983c9de72495238bc8123e13af06
Kauan Nascimento Conceicao <143419130+KauanNC@users.noreply.github.com>
2023-12-06 08:10:01 -0300

    Add files via upload

---
+++ FunçãoQntdImparPar2.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+void verificarQuantidadePar(int numero[])
+{
+    int i, par = 0;
+    
+    for (i = 0; i < 6; i++)
+    {
+        if (numero[i] % 2 == 0)
+        {
+            par++;
+        }
+    }
+    printf("Quantidade de Pares: %d\n", par);
+}
+
+void verificarQuantidadeImpar(int numero[])
+{
+    int impar = 0, i;
+    
+    for (i = 0; i < 6; i++)
+    {
+        if (numero[i] % 2 != 0)
+        {
+            impar++;
+        }
+    }
+    printf("Quantidade de Impares: %d\n", impar);
+}
+void limpar()
+{
+    system("cls||clear");
+}
+
+int main()
+{
+    setlocale(LC_ALL, "portuguese");
+
+    int numero[6];
+    int i;
+
+    for (i = 0; i < 6; i++)
+    {
+        printf("Informe o %d numero:", i + 1);
+        scanf("%d", &numero[i]);
+    }
+
+    limpar();
+
+    for (i = 0; i < 6; i++)
+    {
+        printf("%dº informado: %d\n", i + 1, numero[i]);
+    }
+
+    verificarQuantidadePar(numero);
+    verificarQuantidadeImpar(numero);
+
+    return 0;
+}
